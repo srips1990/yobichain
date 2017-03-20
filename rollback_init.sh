ee179ec73bb63d04b610ce232596af580334a9d1
CYPHER-PC\Sripathi <srips1990@gmail.com>
2017-03-20 19:29:44 +0530

    Added rollback.sh

---
+++ rollback_init.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+
+NC='\033[0m' # No Color
+RED='\033[0;31m'
+LIGHTGREEN='\033[1;32m'
+CYAN='\033[0;36m'
+LIGHTYELLOW='\033[1;33m'
+bold=$(tput bold)
+normal=$(tput sgr0)
+
+echo '----------------------------------------'
+echo -e ${CYAN}${bold}'RESTORING.....'${normal}${NC}
+echo '----------------------------------------'
+
+ps axf | grep 'multichaind' | grep -v grep | awk '{print "kill -9 " $1}' | sh
+rm -rf ~/.multichain/*
+rm -rf /var/www/html/hashchain
+rm -rf /var/www/html/multichain-web-demo
+ps axf | grep 'python -m Mce.abe --config' | grep -v grep | awk '{print "kill -9 " $1}' | sh
+rm -rf /var/www/html/multichain-explorer/
+rm -rf /var/www/html/default_configs
+rm -rf /var/www/html/multichain-1.0-alpha*
+
+echo ''
+echo ''
+echo '----------------------------------------'
+echo ''
+echo ''
+echo ''
+echo ''
