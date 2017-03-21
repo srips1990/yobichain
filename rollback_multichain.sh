30a93a571e1dbe6d04e6357e2d2e4c77379adeb7
CYPHER-PC\Sripathi <srips1990@gmail.com>
2017-03-21 17:51:49 +0530

    Modified home directory for multichain

--- rollback_multichain.sh
+++ rollback_multichain.sh
@@ -8,12 +8,14 @@ LIGHTYELLOW='\033[1;33m'
 bold=$(tput bold)
 normal=$(tput sgr0)
 
+username='yobiuser'
+
 echo '----------------------------------------'
 echo -e ${CYAN}${bold}'RESTORING.....'${normal}${NC}
 echo '----------------------------------------'
 
 ps axf | grep 'multichaind' | grep -v grep | awk '{print "kill -9 " $1}' | sh
-rm -rf ~/.multichain/*
+rm -rf /home/$username/.multichain/*
 rm -rf /var/www/html/hashchain
 rm -rf /var/www/html/multichain-web-demo
 ps axf | grep 'python -m Mce.abe --config' | grep -v grep | awk '{print "kill -9 " $1}' | sh
